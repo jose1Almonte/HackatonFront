263f3ea1b741aada2bb9412e68242cebe8282f02
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-07-07 10:38:47 -0400

    helpers + Conditional Styles + Styles
    Se añadió helpers donde se almacenaran funciones auxiliares, además se añadió la función auxiliar hexToRgba que transforma de hexadecimal a rgba en caso de necesitarse. Se agregaron y modificaron algunos estilos

--- Screens/HomeScreen/HomeScreen.tsx
+++ Screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useCharacters } from '../../hooks/useCharacters';
 import { Card } from '../../components/Card/Card';
 import { textDarkModeStyles, textLightModeStyles } from '../../App';
@@ -12,12 +12,18 @@ function HomeScreen() {
 
 
   return (
-    <ScrollView style={colorScheme === 'dark' ? homeDarkModeStyles.backGround : homeLightModeStyles.backGround}>
-    <Text style={colorScheme === 'dark' ? textDarkModeStyles.title : textLightModeStyles.title}>HomeScreen</Text>
+    <ScrollView 
+    style={colorScheme === 'dark' ? homeDarkModeStyles.backGround : homeLightModeStyles.backGround}
+    contentContainerStyle={homeStyles.backGround}
+    >
+      <Text style={colorScheme === 'dark' ? textDarkModeStyles.title : textLightModeStyles.title}>HomeScreen</Text>
 
-    {characters.map((character, index) => (
+      <View style={homeStyles.cardsContainer}>
+
+      {characters.map((character, index) => (
         <Card character={character} key={index}/>
-    ))}
+        ))}
+      </View>
 
     </ScrollView>
 
@@ -26,7 +32,18 @@ function HomeScreen() {
 
 export default HomeScreen;
 
+const homeStyles = StyleSheet.create({
 
+  backGround:{
+    alignItems: 'center',
+  },
+
+  cardsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+});
 
 const homeLightModeStyles = StyleSheet.create({
 
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,32 +2,66 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
 import { textDarkModeStyles, textLightModeStyles } from '../../App';
 import useColorScheme from '../../hooks/useColorScheme';
+import { hexToRGBA } from '../../helpers/convertionHelpers';
 
 export const Card = ({character}:{character: any}) => {
 
     const {colorScheme} = useColorScheme();
 
     return (
-    <View>
+    <View style={colorScheme === 'dark' ? [cardDarkStyles.containerCard, cardStyles.containerCard] : [cardLightStyles.containerCard, cardStyles.containerCard]}>
+        <View style={cardStyles.textBox}>
+
         <Text style={colorScheme === 'dark' ? textDarkModeStyles.title : textLightModeStyles.title}>Hello {character.name}</Text>
         <Text style={colorScheme === 'dark' ? textDarkModeStyles.title : textLightModeStyles.title}>{character.id}</Text>
-        <Image style={colorScheme === 'dark' ? cardStyles.imageStyles : cardStyles.imageStyles} source={{ uri: character.image }} />
+
+        </View>
+
+        <Image style={cardStyles.imageStyles} source={{ uri: character.image }} />
     </View>
   );
 };
 
 const cardStyles = StyleSheet.create({
     imageStyles:{
-        height: 100,
-        width: 100,
+        height: 'auto',
+        aspectRatio: '1.1',
+    },
+
+    containerCard: {
+        width: '70%',
+        height: 125,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 5,
+        marginHorizontal: '1%',
+        borderRadius: 5,
+
+        overflow: 'hidden',
+    },
+
+    textBox:{
+        width: '50%',
+        // backgroundColor: 'red',
+        // overflow: 'hidden',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
     },
 });
 
 const cardLightStyles = StyleSheet.create({
     containerCard: {
-        width: '90%',
-        height: 'auto',
-        backgroundColor: 'grey',
+        overflow: 'hidden',
+        backgroundColor: hexToRGBA('#000000', 0.6),
+
+    },
+});
+
+const cardDarkStyles = StyleSheet.create({
+    containerCard: {
+        // width: '70%',
+        // height: 'auto',
+        backgroundColor: hexToRGBA('#FFFFFF', 0.6),
 
     },
 });
